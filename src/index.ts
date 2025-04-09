dba8ec270f1a1ffed2ffb22abbac1437ff3220c6
maretol <maretol@outlook.com>
2025-04-09 21:40:12 +0900

    rootをホームページに転送するように変更

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.redirect("https://www.maretol.xyz", 308)
 })
 
 export default app
